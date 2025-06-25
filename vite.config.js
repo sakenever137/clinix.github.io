4b92d1414b2dbdf481b90dc2b8aa4294505cc588
sakenserdaly <sakenever137@gmail.com>
2025-06-25 15:51:16 +0500

    фвв

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/clinix.github.io/', // Публикация в корень
+  base: '/clinix.github.io/',  // для корректных путей к ресурсам
   plugins: [react()],
 })
